15b80b3d07c4d0c7c5db6805d27385ec10a432b4
HASAN MAHMUD <123263804+MUHENDIShasanMAHMUD@users.noreply.github.com>
2023-02-28 11:38:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dortgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y; //bizim kenarlarımız girdik .
+            Console.WriteLine("ENTER X :  "); //userden kısa kenar girmesini istedik .
+            x = double.Parse(Console.ReadLine()); // bizim bilinmeyeli X user tarafından girdiği değer verdik. 
+            Console.WriteLine("ENTER THE SECOND LEGENTH : "); //userden uzun kenar girmesini istedik .
+            y = double.Parse(Console.ReadLine()); // bizim bilinmeyeli Y user tarafından girdiği değer verdik. 
+            Console.WriteLine("THE RECTANGLE AREA İS : ");
+            Console.WriteLine(x * y); //dörtgenin alanı hesapladık ve ekran çıktısında yazdık.
+            Console.ReadKey(); //konsul kapatmması için.
+        }
+    }
+}
